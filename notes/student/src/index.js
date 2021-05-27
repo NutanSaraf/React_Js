550acbbbf34bfdbd193d0ecce9a6751362eb72cd
praveen <itasworkshop@gmail.com>
2021-05-27 21:19:57 +0530

    27 May

--- notes/student/src/index.js
+++ notes/student/src/index.js
@@ -1,44 +1,86 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class StudentForm extends React.Component{
+function CurrencyMessage(props){
+  if(props.rupees >= 1000){
+    return <p>this is too expensive.</p>
+  }
+  return <p>No this is not too expensive.</p>
+}
+
+const unitName = {
+  r: 'rupees',
+  d: 'dollar'
+};
+
+function toRupees(rupees){
+  return (rupees*75);
+}
+
+function toDollar(rupees){
+  return (rupees/75);
+}
+
+class CurrenyInput extends React.Component{
   constructor(props){
     super(props);
-    this.state = { isPass: true, noOfAttempts:5};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    //this.state = { currency: ''}; not good idea because no connection between two component
+    this.handleChange = this.handleChange.bind(this);    
   }
 
   handleChange(event){
-    //this.setState({value: event.target.value});
-    const target = event.target;
-    const value = target.name === "isPass" ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({[name]:value});
-  }
-
-  handleSubmit(event){
-    alert('Are you sure you want to submit form?'+this.state.isPass +" "+this.state.noOfAttempts);
-    event.preventDefault();
+    //this.setState({currency : event.target.value});
+    this.props.onCurrencyChange(event.target.value);
   }
 
   render(){
+    //const currency = this.state.currency;
+    const currency = this.props.currency;
+    const unit = this.props.unit;
+
     return(
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Is pass:
-            <input name="isPass" type="checkbox" checked={this.state.isPass} onChange={this.handleChange} />
-          </label>
-          <label>
-            <input name="noOfAttempts" type="number" value={this.state.noOfAttempts} onChange={this.handleChange} />
-          </label>
-            <input type="submit" value="Submit" />
-        </form>
+      <fieldset>
+        <legend>Enter your currency in {unitName[unit]}</legend>
+        <input value={currency} onChange={this.handleChange}/>
+        <CurrencyMessage rupees={currency} />
+      </fieldset>
+
+    );
+  }
+}
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { currency: '' , unit: 'r'};
+    this.handleDollarChange = this.handleDollarChange .bind(this); 
+    this.handleRupeeChange = this.handleRupeeChange .bind(this); 
+  }
+
+  handleRupeeChange(currency){
+    this.setState({'unit':'r', currency})
+  }
+
+  handleDollarChange(currency){
+    this.setState({'unit':'d', currency})
+  }
+
+
+  render(){
+    const currency = this.state.currency;
+    const unit = this.state.unit;
+    const rupees = unit === 'd' ? toRupees(currency) : currency;
+    const dollar = unit === 'r' ? toDollar(currency) : currency;
+
+    return(
+      <div>
+        <CurrenyInput unit='r' currency={rupees} onCurrencyChange={this.handleRupeeChange}/>
+        <CurrenyInput unit='d' currency={dollar} onCurrencyChange={this.handleDollarChange}/>
+      </div>
     );
   }
 }
 
 ReactDOM.render(
-  <StudentForm />, document.getElementById('root')
+  <Calculator />, document.getElementById('root')
  );
